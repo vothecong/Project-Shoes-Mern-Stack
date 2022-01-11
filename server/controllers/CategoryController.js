f098aa049ec9cf9407e1fb4db78c2511c43fbab0
vothecong <vtconga4tqtpy@gmail.com>
2022-01-11 22:10:14 +0700

    update

--- server/controllers/CategoryController.js
+++ server/controllers/CategoryController.js
@@ -4,122 +4,118 @@ const { removeAccents } = require("../configs/CONFIGS");
 const Product = require("../models/ProductModel");
 
 const getCategories = (req, res, next) => {
-    Category.find()
-        .then((result) => {
-            return res.status(200).json(result);
-        }).catch((err) => {
-            res.status(500).json({
-                error: err
-            })
-        });
-}
+    try {
+        const result = await Category.find().exec();
+        return res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
 
 const postCategory = (req, res, next) => {
-    if (req.user.role === 'admin') {
+    if (req.user.role === "admin") {
         const { name, image } = req.body;
         Category.findOne({ slug: slugify(removeAccents(name)) })
             .then((result) => {
                 if (result) {
                     return res.status(404).json({
-                        error: "Tên sản phẩm đã tồn tại!!!"
+                        error: "Tên sản phẩm đã tồn tại!!!",
                     });
-                }
-                else {
+                } else {
                     const newCategory = new Category({
                         name,
                         slug: slugify(removeAccents(name)),
-                        image: image
+                        image: image,
                     });
-                    newCategory.save()
+                    newCategory
+                        .save()
                         .then((data) => {
                             return res.status(201).json({
                                 success: true,
-                                data
+                                data,
                             });
-                        }).catch((err) => {
+                        })
+                        .catch((err) => {
                             res.status(500).json({
-                                error: err
-                            })
+                                error: err,
+                            });
                         });
                 }
-            }).catch((err) => {
+            })
+            .catch((err) => {
                 res.status(500).json({
-                    error: err
-                })
+                    error: err,
+                });
             });
     }
-}
+};
 
 const getCategory = (req, res, next) => {
-    Category.findById({ _id: req.params.id })
-        .then((result) => {
-            return res.status(200).json(result);
-        }).catch((err) => {
-            res.status(500).json({
-                error: err
-            })
-        });
-}
+    try {
+        const result = await Category.findById({ _id: req.params.id });
+        return res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
 
 const deleteCategory = async (req, res, next) => {
     try {
-        if (!Object.is(req.user.role, 'admin')) {
+        if (!Object.is(req.user.role, "admin")) {
             return res.status(404).json({
                 error: "Không tìm thấy tài khoản!!",
-                success: false
+                success: false,
             });
         }
-        if (Object.is(req.user.role, 'admin')) {
+        if (Object.is(req.user.role, "admin")) {
             try {
                 const { id } = req.params;
                 const category = await Category.findOne({ _id: id });
                 if (!category) {
                     return res.status(404).json({
-                        error: "Đã xảy ra lỗi hoặc không tìm thấy loại sản phẩm!!"
+                        error: "Đã xảy ra lỗi hoặc không tìm thấy loại sản phẩm!!",
                     });
                 }
                 let result = await category.deleteOne();
                 let data = await Product.deleteMany({ categoryID: result._id });
                 if (!data) {
                     return res.status(404).json({
-                        error: "Đã xảy ra lỗi hoặc không tìm thấy sản phẩm!!"
+                        error: "Đã xảy ra lỗi hoặc không tìm thấy sản phẩm!!",
                     });
                 }
                 return res.status(200).json(result);
             } catch (error) {
                 return res.status(500).json({
-                    error: err
+                    error: err,
                 });
             }
         }
     } catch (error) {
-        res.status(500).json({
-            error: error
-        })
+        next(error);
     }
-
-}
+};
 
 const updateCategory = async (req, res, next) => {
     const { name, image } = req.body;
     try {
-        if (req.user.role === 'admin') {
+        if (req.user.role === "admin") {
             const result = await Category.findOneAndUpdate(
                 { _id: req.params.id },
                 {
                     $set: {
                         name: name,
                         slug: slugify(removeAccents(name)),
-                        image: image
-                    }
-                }, { new: true }
+                        image: image,
+                    },
+                },
+                { new: true }
             );
             return res.status(200).json(result);
         }
     } catch (error) {
         next(error);
     }
-}
+};
 
 const search = async (req, res, next) => {
     const { search } = req.body;
@@ -131,7 +127,7 @@ const search = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+};
 
 module.exports = {
     getCategories,
@@ -140,5 +136,4 @@ module.exports = {
     deleteCategory,
     updateCategory,
     search,
-}
-
+};
